17ee884d56fa0d4583a31e78ba04f8b653e5dcd3
Weiqi Ji <jiweiqi717@gmail.com>
2018-11-27 12:15:49 -0500

    changed some stuff

--- mySite.js
+++ mySite.js
@@ -17,3 +17,14 @@ function stop2() {
     var audio2 = document.getElementById("audio2");
     audio2.pause();
 }
+
+var myVar;
+
+function myFunction() {
+    myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
