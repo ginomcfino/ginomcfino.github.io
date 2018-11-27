84b6b4462347af0e4d2a6af608781c4c583f3b75
Weiqi Ji <jiweiqi717@gmail.com>
2018-11-27 11:02:50 -0500

    almost done

--- mySite.js
+++ mySite.js
@@ -2,3 +2,18 @@ function play() {
     var audio = document.getElementById("audio");
     audio.play();
 }
+
+function stop() {
+    var audio = document.getElementById("audio");
+    audio.pause();
+}
+
+function play2() {
+    var audio2 = document.getElementById("audio2");
+    audio2.play();
+}
+
+function stop2() {
+    var audio2 = document.getElementById("audio2");
+    audio2.pause();
+}
